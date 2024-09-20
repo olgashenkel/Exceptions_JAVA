07a3c9997b5c14483dff2c226433d64a24b93e46
olga_shenkel <olga_shenkel@mail.ru>
2024-09-20 18:50:00 +0200

    add Exceptions_JAVA/Seminar_3

--- Seminar_3/src/DZ.java
+++ Seminar_3/src/DZ.java
@@ -21,5 +21,59 @@ java и создать свои. Исключение должно быть ко
  */
 
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class DZ {
+    private static final int fields_number = 6;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+
+        String[] fields = input.split(" ");
+        if (fields.length != fields_number) {
+            System.out.println("Неверное количество полей, вы ввели " + fields.length + " - а необходимо 6!");
+        }
+        String lastName = fields[0];
+        String firstName = fields[1];
+        String middleName = fields[2];
+
+        LocalDate birthDate;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            birthDate = LocalDate.parse(fields[3], formatter);
+        } catch (DateTimeParseException e) {
+            System.out.println("Неверный формат даты!");
+            return;
+        }
+
+        long phoneNumber;
+        try {
+            phoneNumber = Long.parseLong(fields[4]);
+        } catch (NumberFormatException e) {
+            System.err.println("Неверный формат телефона!");
+            return;
+        }
+
+        String gender = fields[5];
+        if ((!"m".equals(gender)) && (!"f".equals(gender))) {
+            System.err.println("Неверный формат пола, введи f или m!");
+            return;
+        }
+
+        String fileName = lastName + ".txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            writer.write(lastName + " " + firstName + " " + middleName + " " + birthDate.format(DateTimeFormatter.ISO_LOCAL_DATE) + " " + phoneNumber + " " + gender);
+            writer.newLine();
+        }catch (IOException e){
+            System.out.println("Ошибка записи!");
+        }
+    }
 }
